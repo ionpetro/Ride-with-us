492a6d4ab252165877ba5742269d4a462afdea7b
ionpetro <ionpetro@gmail.com>
2023-02-06 10:15:14 +0200

    Activities

---
+++ components/HomePage/Activities/Activities.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+
+const Activities = () => {
+  const activities = [
+    {
+      title: "Παρνασσός",
+      image_url: "/assets/images/sports/parnassos.jpg",
+      link: "/activities/parnassos",
+      price: 50,
+    },
+    {
+      title: "Βασιλίτσα",
+      image_url: "/assets/images/sports/vasilitsa.jpg",
+      link: "/activities/vasilitsa",
+      price: 165,
+    },
+    {
+      title: "Καλάβρυτα",
+      image_url: "/assets/images/sports/kalavrita.jpg",
+      link: "/activities/kalavrita",
+      price: 50,
+    },
+    {
+      title: "Παύλιανη",
+      image_url: "/assets/images/sports/pavliani.jpg",
+      link: "/activities/pavliani",
+      price: 50,
+    },
+    {
+      title: "Καρδίτσα",
+      image_url: "/assets/images/sports/karditsa.jpg",
+      link: "/activities/karditsa",
+      price: 130,
+    },
+  ];
+  return (
+    <>
+      {/* Start Card Style */}
+      <div className="rbt-course-card-area rbt-section-gap bg-color-white">
+        <div className="container">
+          <div className="row align-items-center mb--60">
+            <div className="col-lg-12">
+              <div className="section-title text-center">
+                <span className="subtitle bg-secondary-opacity">
+                  Δραστηριοτητες
+                </span>
+                <h2 className="title">Όλες οι δραστηριότητες</h2>
+                <p className="description has-medium-font-size mt--20">
+                  Εκδρομές κάθε βδομάδα
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* Start Card Area */}
+          <div className="row g-5">
+            {activities.map(({ title, image_url, link, price }) => (
+              <div
+                className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12"
+                data-sal-delay={150}
+                data-sal="slide-up"
+                data-sal-duration={800}
+                key={title}
+              >
+                <div className="rbt-card variation-03 rbt-hover">
+                  <div className="rbt-card-img">
+                    <a className="thumbnail-link" href={link}>
+                      <img src={image_url} alt={title} />
+                      <span className="rbt-btn btn-white icon-hover btn-md">
+                        <span className="btn-text">Περισσότερα</span>
+                        <span className="btn-icon">
+                          <i className="feather-arrow-right" />
+                        </span>
+                      </span>
+                    </a>
+                  </div>
+                  <div className="rbt-card-body">
+                    <h5 className="rbt-card-title">
+                      <a href={link}>{title}</a>
+                    </h5>
+                    <div className="rbt-card-bottom">
+                      <a className="transparent-button" href={link}>
+                        <i>
+                          <svg
+                            width={17}
+                            height={12}
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <g stroke="#27374D" fill="none" fillRule="evenodd">
+                              <path d="M10.614 0l5.629 5.629-5.63 5.629" />
+                              <path
+                                strokeLinecap="square"
+                                d="M.663 5.572h14.594"
+                              />
+                            </g>
+                          </svg>
+                        </i>
+                      </a>
+                    </div>
+                  </div>
+                  <div className="card-information">
+                    <div className="card-count">
+                      από <strong>{price}</strong> €
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Activities;
--- components/HomePage/HomePage.jsx
+++ components/HomePage/HomePage.jsx
@@ -1,98 +1,13 @@
-import Image from "next/image";
 import React from "react";
-import styles from "./HomePage.module.scss";
+import Activities from "./Activities/Activities";
+import Banner from "./Banner/Banner";
 
 const HomePage = () => {
   return (
-    <div className="rbt-banner-area rbt-banner-7 bg-gradient-1 theme-shape header-transperent-spacer">
-      <div className="wrapper w-100">
-        <div className="container">
-          <div className="row g-5 justify-content-between align-items-center">
-            <div className="col-lg-6 order-2 order-lg-1">
-              <div className="content">
-                <div className="inner">
-                  <div className="section-title text-start">
-                    <span className="subtitle bg-primary-opacity">
-                      Fun excursions & more
-                    </span>
-                  </div>
-                  <h1 className="title">
-                    An engaged{" "}
-                    <span className="theme-gradient">X-treme sports</span>{" "}
-                    community
-                  </h1>
-                  <div className="rbt-like-total">
-                    <div className="profile-share">
-                      <a
-                        href="#"
-                        className="avatar"
-                        data-tooltip="Παρνασσός"
-                        tabIndex={0}
-                      >
-                        <img
-                          src="/assets/images/sports/parnassos.jpg"
-                          alt="education"
-                          style={{ height: "60px" }}
-                        />
-                      </a>
-                      <a
-                        href="#"
-                        className="avatar"
-                        data-tooltip="Καλάβρυτα"
-                        tabIndex={0}
-                      >
-                        <img
-                          src="/assets/images/sports/kalavrita.jpg"
-                          alt="education"
-                          style={{ height: "60px" }}
-                        />
-                      </a>
-                      <a
-                        href="#"
-                        className="avatar"
-                        data-tooltip="Βασιλίτσα"
-                        tabIndex={0}
-                      >
-                        <img
-                          src="/assets/images/sports/vasilitsa.jpg"
-                          alt="education"
-                          style={{ height: "60px" }}
-                        />
-                      </a>
-                      <div className="more-author-text">
-                        <h5 className="total-join-students">Έλα μαζί μας</h5>
-                        <p className="subtitle">
-                          Νέες δραστηριότητες έρχονται σύντομα
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="rbt-button-group justify-content-start mt--30">
-                    <a className="rbt-btn btn-gradient rbt-switch-btn" href="#">
-                      <span data-text="Όλες οι δραστηριότες">
-                        Όλες οι δραστηριότες
-                      </span>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-6 order-1 order-lg-2">
-              <div className="thumbnail-wrapper">
-                <div className={`thumbnail text-end ${styles.imageWrapper}`}>
-                  <Image
-                    src="/assets/images/sports/snowboard.jpg"
-                    alt="Education Images"
-                    layout="fill"
-                    objectFit="cover"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <Banner />
+      <Activities />
+    </>
   );
 };
 
